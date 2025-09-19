a0c1a2dea75ca0c375a62f978415525a3ea5ad28
Vaishnavi <70702032+Vaishnavi280@users.noreply.github.com>
2025-09-19 23:56:16 +0100

    Add files via upload

---
+++ MavenBasicTest.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MavenBasicTest {
+
+	@Test
+	public void test() {
+		  assertTrue(true);;
+	}
+
+}
---
+++ MavenClassBasic.java
@@ -0,0 +1,11 @@
+package com.example.Maven;
+
+public class MavenClassBasic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Basic Maven Project built to test the basic maven commands");
+
+	}
+
+}
